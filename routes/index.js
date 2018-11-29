a3728c6bb29ebed28263dec80c139432aab0b33b
Albert Moreira <albertm@take.net>
2018-11-29 17:28:50 -0200

    Configuring data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var data = {
+  title: "Site title",
+  tagLine: "Main Tagline",
+  mainNav: [
+    { label: 'Teste', link: '#about' },
+    { label: 'Teste', link: '#about' }
+  ],
+  about: {
+    title: "About",
+    content: "This site was built to show how express and EJS works together."
+  },
+  products: {
+    title: "Products",
+    content: "<p>Teste</p><p>Texto de teste da página</p>"
+  }
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', data);
 });
 
 module.exports = router;
