baccd07a1075a1fc3ae1359d16eb9e9f349e1a21
Albert Moreira <albertm@take.net>
2018-11-30 16:07:36 -0200

    Reponsive webnav

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ var router = express.Router();
 
 var data = {
   title: "Site title",
-  tagLine: "Main Tagline",
+  cover: {
+    content: "<h1>Simple and beautiful website template</h1>"
+  },
   mainNav: [
-    { label: 'Teste', link: '#about' },
-    { label: 'Teste', link: '#about' }
+    { label: 'About', route: '#about' },
+    { label: 'Products', route: '#products' }
   ],
   about: {
     title: "About",
@@ -14,7 +16,13 @@ var data = {
   },
   products: {
     title: "Products",
-    content: "<p>Teste</p><p>Texto de teste da página</p>"
+    content: "<p>Teste</p><p>Texto de teste da página</p>",
+    list: [
+      { id: 1, title: "Produto 1", description: "Esse é o primeiro produto", route: "/products/1", image: "/img/test-image.png" },
+      { id: 2, title: "Produto 2", description: "Esse é o segundo produto", route: "/products/2", image: "/img/test-image.png" },
+      { id: 3, title: "Produto 3", description: "Esse é o terceiro produto", route: "/products/3", image: "/img/test-image.png" },
+      { id: 4, title: "Produto 4", description: "Esse é o quarto produto", route: "/products/4", image: "/img/test-image.png" },
+    ]
   }
 }
 
